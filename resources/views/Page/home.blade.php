be2d681c12e24742280e217907621740870868b8
hphuocc <nguyenphuoc0750@gmail.com>
2025-04-15 21:24:32 +0700

    sửa lỗi

--- resources/views/Page/home.blade.php
+++ resources/views/Page/home.blade.php
@@ -171,7 +171,7 @@
           <nav class="sidebar sidebar-offcanvas" id="sidebar">
             <ul class="nav">
               <li class="nav-item active">
-                <a class="nav-link" href="admin">
+                <a class="nav-link" href="home">
                   <i class="mdi mdi-grid-large menu-icon"></i>
                   <span class="menu-title">Dashboard</span>
                 </a>
